02797ae9950cb2fa61f3f89161456c6a27254f95
rdm10br <43587845+rdm10br@users.noreply.github.com>
2019-06-01 21:43:11 -0300

    Update valida1.js

--- Front-End/valida1.js
+++ Front-End/valida1.js
@@ -1,6 +1,6 @@
 function login(){
   //alert('teste'); 
-  var attempt = 3;
+  //var attempt = 3;
   var un = document.getElementById('DropdownFormEmail2').value;
   var pwrd = document.getElementById('DropdownFormPassword1').value;
   if (un == "" && pwrd == "") 
@@ -21,11 +21,11 @@ function login(){
 //    attempt --;
 //    return false;
 //   }
-  if(attempt == 0)
-  {
-   document.getElementById("DropdownFormEmail2").disabled = true;
-   document.getElementById("DropdownFormPassword1").disabled = true;
-   document.getElementById("submit01").disabled = true;
-   return false;
-  }
+//   if(attempt == 0)
+//   {
+//    document.getElementById("DropdownFormEmail2").disabled = true;
+//    document.getElementById("DropdownFormPassword1").disabled = true;
+//    document.getElementById("submit01").disabled = true;
+//    return false;
+//  }
 }
