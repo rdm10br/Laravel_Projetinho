5efae8c59af8b795dbd003ef65db3aeeb849c362
rdm10br <43587845+rdm10br@users.noreply.github.com>
2019-06-01 21:27:15 -0300

    Update valida1.js

--- Front-End/valida1.js
+++ Front-End/valida1.js
@@ -3,15 +3,21 @@ function login(){
   var attempt = 3;
   var un = document.getElementById('DropdownFormEmail2').value;
   var pwrd = document.getElementById('DropdownFormPassword1').value;
-  if (un == null && pwrd == "") 
+  if (un == "" && pwrd == "") 
   {
    alert("Por Favor preencha corretamente o email.");
    attempt --;
    return false;
   }
-  if (pwrd == null && un == "") 
+  if (un == "" && pwrd == "") 
   {
    alert("Por Favor preencha corretamente a senha.");
+   attempt --;
+   return false;
+  }
+  if(un == "" && pwrd== "")
+  {
+   alert('Por Favor preencha correto os campos');
    attempt --;
    return false;
   }
