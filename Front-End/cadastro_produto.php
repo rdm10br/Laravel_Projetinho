f61b64c892b923f7a76c7fc0ebfd4943273f9369
Breno <50470750+CapivaraMaluca@users.noreply.github.com>
2019-06-01 22:43:37 -0300

    asdada

--- Front-End/cadastro_produto.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include_once("conexao.php");
-    $nome = mysqli_real_escape_string($conn, $_POST('nome'));
-    $descricao = mysqli_real_escape_string($conn, $_POST('descricao'));
-    $imagem = mysqli_real_escape_string($conn, $_POST('imagem'));
-?>
