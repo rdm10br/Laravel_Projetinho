4758b90f179fcefc76024383837abe602a77bf9a
rdm10br <43587845+rdm10br@users.noreply.github.com>
2019-06-01 21:01:19 -0300

    att2

--- Front-End/valida1.js
+++ Front-End/valida1.js
@@ -1,3 +1,24 @@
 function login(){
-   window.alert("aiai");
+  var attempt = 3;
+  var un = document.getElementById('DropdownFormEmail2').value;
+  var pwrd = document.getElementById('DropdownFormPassword1').value;
+  if (un == null && pwrd == "") 
+  {
+   alert("Por Favor preencha corretamente o email.");
+   attempt --;
+   return false;
+  }
+  if (pwrd == null && un == "") 
+  {
+   alert("Por Favor preencha corretamente a senha.");
+   attempt --;
+   return false;
+  }
+  if(attempt == 0)
+  {
+   document.getElementById("DropdownFormEmail2").disabled = true;
+   document.getElementById("DropdownFormPassword1").disabled = true;
+   document.getElementById("submit01").disabled = true;
+   return false;
+  }
 }
